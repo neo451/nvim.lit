ba3062a7938bb999d2adacc0f450060a9d494a4e
neo451 <412444506@qq.com>
2025-09-03 21:49:48 +0800

    remove!

--- lua/_octo.lua
+++
@@ -1,6 +0,0 @@
-require "octo".setup {
-  enable_builtin = true,
-  -- default_to_projects_v2 = true,
-  default_merge_method = "squash",
-  picker = "snacks",
-}
--- lua/_oil.lua
+++
@@ -1,9 +0,0 @@
-require "oil".setup {
-  skip_confirm_for_simple_edits = true,
-  win_options = {
-    signcolumn = "yes:2",
-  },
-  view_options = {
-    show_hidden = true,
-  },
-}
